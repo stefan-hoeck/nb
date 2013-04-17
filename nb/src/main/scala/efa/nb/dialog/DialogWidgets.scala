61144b2bc1ffcc6552e10e050da8021e7a2b1e68
gundi <hock@zhaw.ch>
2013-04-17 07:22:03 +0200

    removed more stuff

--- nb/src/main/scala/efa/nb/dialog/DialogWidgets.scala
+++ nb/src/main/scala/efa/nb/dialog/DialogWidgets.scala
@@ -1,25 +1,8 @@
 package efa.nb.dialog
 
-//import efa.core._, Efa._
-//import efa.nb.VSIn
-//import efa.react.{SIn, Out, sTrans, AsSignal}
-//import efa.react.swing.AllFunctions._
-//import scala.swing._
-//import scalaz._, Scalaz._, effect.IO
-//
 //trait DialogWidgets extends DialogWidgetsInstances with DialogWidgetsFunctions
 //
 //trait DialogWidgetsFunctions {
-//  def validate[A,B](in: VSIn[A])(v: Validator[A,B]): VSIn[B] =
-//    in map (_.disjunction flatMap v.run validation)
-//
-//  def valIO[A,B](in: VSIn[A])(v: A ⇒ IO[ValRes[B]]): VSIn[B] =
-//    in mapIO (_ traverse v map (_ flatMap identity))
-//
-//  def valIn[A](in: SIn[A]): VSIn[A] = in ∘ (_.success)
-//
-//  def sin[A,B](a: A)(implicit S: AsSignal[A,B]): VSIn[B] = 
-//    valIn(sTrans in a)
 //
 //  def checkBox (cb: CheckBox): VSIn[Boolean] = valIn(selectedS(cb))
 //
@@ -60,30 +43,5 @@ package efa.nb.dialog
 //  ): VSIn[A] = valIO(textIn[A](t))(v)
 //
 //}
-//
-//trait DialogWidgetsInstances {
-//  import DialogWidgets.valIn
-//
-//  implicit val VSInFunctor: Functor[VSIn] =
-//    Functor[SIn].compose[ValRes]
-//
-//  implicit val VSInApplicative: Applicative[VSIn] =
-//    Applicative[SIn].compose[ValRes]
-//
-//  implicit def CheckBoxSIn (b: CheckBox): VSIn[Boolean] =
-//    valIn(selectedS(b))
-//
-//  implicit def ComboBoxSIn[A] (b: ComboBox[A]): VSIn[A] =
-//    valIn(itemS(b))
-//
-//  implicit def LabelOutString (l: Label): Out[String] = text(l)
-//
-//  implicit def LabelOutShows[A:Show] (l: Label): Out[A] = text(l) ∙ (_.shows)
-//
-//  implicit def PasswordSIn (b: PasswordField): VSIn[String] =
-//    valIn(passwordS(b))
-//}
-//
-//object DialogWidgets extends DialogWidgets
 
 // vim: set ts=2 sw=2 et:
