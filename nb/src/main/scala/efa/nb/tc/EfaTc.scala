81d443ae368f100ab4cfacfc09a7de61f98bc40d
efasckenoth <efasckenoth@gmail.com>
2013-04-18 09:35:54 +0200

    reimplemented EfaTc

--- nb/src/main/scala/efa/nb/tc/EfaTc.scala
+++ nb/src/main/scala/efa/nb/tc/EfaTc.scala
@@ -1,28 +1,38 @@
 package efa.nb.tc
 
-//import org.openide.windows.{TopComponent, WindowManager}
-//import org.openide.util.{Lookup, NbPreferences}
-//import java.util.prefs.Preferences
-//
-//abstract class EfaTc extends TopComponent with PersistentComponent {
-//  
-//  import EfaTc.reg
-//
-//  reg = this :: reg
-//  
-//  override def componentOpened() { read.unsafePerformIO }
-//  
-//  override def componentClosed() { persist.unsafePerformIO }
-//  
-//  override def getPersistenceType = TopComponent.PERSISTENCE_ALWAYS
-//  
-//  protected[tc] def prefId = preferredID
-//
-//}
-//
-//object EfaTc {
-//  private var reg: List[EfaTc] = Nil
-//  private[tc] def registry = reg
-//}
+import org.openide.windows.{TopComponent}
+import org.openide.util.{Lookup, NbPreferences}
+import java.util.prefs.Preferences
+import scala.collection.mutable.{ArrayBuffer, SynchronizedBuffer}
+import scalaz._, Scalaz._, effect.IO
+
+abstract class EfaTc extends TopComponent with PersistentComponent {
+  protected def initialize: IO[Unit]
+
+  protected def cleanup: IO[Unit]
+
+  final private[tc] def doOpen = 
+    initialize >> read >> EfaTc.add(this)
+
+  final private[tc] def doClose = 
+    EfaTc.remove(this) >> persist >> cleanup
+
+  override def componentOpened() { doOpen.unsafePerformIO() }
+  
+  override def componentClosed() { doClose.unsafePerformIO() }
+  
+  override def getPersistenceType = TopComponent.PERSISTENCE_ALWAYS
+  
+  protected[tc] def prefId = preferredID
+}
+
+object EfaTc {
+  private[this] val reg = new ArrayBuffer[EfaTc] with SynchronizedBuffer[EfaTc]
+  private[tc] def registry: IO[List[EfaTc]] = IO(reg.toList)
+
+  private def add(tc: EfaTc) = IO(reg += tc)
+
+  private def remove(tc: EfaTc) = IO(reg -= tc)
+}
 
 // vim: set ts=2 sw=2 et:
--- nb/src/main/scala/efa/nb/tc/install/Installer.scala
+++ nb/src/main/scala/efa/nb/tc/install/Installer.scala
@@ -1,7 +1,14 @@
 package efa.nb.tc.install
 
+import efa.nb.tc.EfaTc
+import scalaz._, Scalaz._
+
 class Installer extends efa.nb.module.NbModule {
-  protected def closeIO = efa.nb.NbSystem.shutdown map { _ ⇒ true }
+  protected def closeIO = for {
+    tcs ← EfaTc.registry
+    _   ← tcs foldMap { _.doClose }
+    _   ← efa.nb.NbSystem.shutdown
+  } yield true 
 }
 
 // vim: set ts=2 sw=2 et:
