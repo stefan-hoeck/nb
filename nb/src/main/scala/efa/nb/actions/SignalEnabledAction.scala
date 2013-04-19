03361b772bb6c8ad9a532cc5425364578a1e31e9
stefan-hoeck <efasckenoth@gmail.com>
2013-04-19 06:33:41 +0200

    reimplemented SignalEnabledAction

--- nb/src/main/scala/efa/nb/actions/SignalEnabledAction.scala
+++ nb/src/main/scala/efa/nb/actions/SignalEnabledAction.scala
@@ -1,12 +1,14 @@
 package efa.nb.actions
 
+import efa.nb.NbSystem.forever
+import dire.SIn
 import javax.swing.Action
 import scalaz.effect.IO
 
 trait SignalEnabledAction extends Action {
-//  protected def enabledIn: SIn[Boolean]
-//
-//  enabledIn to (b ⇒ IO(setEnabled(b))) runIO () unsafePerformIO
+  protected def enabledIn: SIn[Boolean]
+
+  forever(enabledIn syncTo { b ⇒ IO(setEnabled(b)) }).unsafePerformIO()
 }
 
 // vim: set ts=2 sw=2 et:
