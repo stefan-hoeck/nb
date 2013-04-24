768cef26e3cb0d23a4e42c357a5cbe8eed6b22c7
stefan-hoeck <efasckenoth@gmail.com>
2013-04-24 07:03:46 +0200

    started with InputWidgets

--- nb/src/main/scala/efa/nb/InputWidgets.scala
+++ nb/src/main/scala/efa/nb/InputWidgets.scala
@@ -6,42 +6,39 @@ import efa.core._, Efa._
 import scalaz._, Scalaz._
 
 trait WidgetsFunctions extends ValidationFunctions {
+
+//  def checkBox[A] (b: CheckBox)(l: A @> Boolean): VStSF[A,A] =
+//    lensed(b.value >=> success)(l)
 //
-//  def checkBox[A] (b: CheckBox)(l: A @> Boolean): VSET[A,A] =
-//    lensed(values(b) >=> success)(l)
-//
-//  def comboBox[A,B] (b: ComboBox[B])(l: A @> B): VSET[A,A] =
+//  def comboBox[A,B] (b: ComboBox[B])(l: A @> B): VStSF[A,A] =
 //    lensed(values(b) >=> success)(l)
 
   def getSet[A,B,C](get: A ⇒ C)(set: (A, C) ⇒ ValSt[B], in: SfV[C,C])
-    : VStSF[A,B] = {
-    def vset (a: A, vi: ValRes[C]) = vi flatMap (set(a, _))
-
-    SF.id[A].upon(in ∙ get)(vset)
-  }
+    : VStSF[A,B] =
+    SF.id[A].upon(in ∙ get){(a,vi) ⇒ vi flatMap (set(a,_)) }
 //
 //  def intIn[A](
 //    t: TextField,
 //    v: EndoVal[Int] = Validators.dummy[Int]
-//  )(l: A @> Int): VSET[A,A] = textIn[A,Int](t, v = v)(l)
+//  )(l: A @> Int): VStSF[A,A] = textIn[A,Int](t, v = v)(l)
 //
-//  def lensed[A,B] (in: ValSET[B,B])(l: A @> B): VSET[A,A] =
-//    in map (_ map (l := _ void)) contramap l.get
-//
-//  def lensedV[A,B] (in: VSET[B,B])(l: A @> B): VSET[A,A] =
-//    mapSt(in)(l) contramap l.get
-//
-//  def mapSt[A,B,C] (in: VSET[A,B])(l: C @> B): VSET[A,C] = {
-//    def nextSt (s: State[B,Unit]): State[C,Unit] =
-//      init[C] >>= (c ⇒ l := s.exec (l get c)) void
-//
-//    in map (_ map nextSt)
-//  }
-//
+  def lensed[A,B](in: SfV[B,B])(l: A @> B): VStSF[A,A] =
+    in map (_ map (l := _ void)) contramap l.get
+
+  def lensedV[A,B] (in: VStSF[B,B])(l: A @> B): VStSF[A,A] =
+    mapSt(in)(l) contramap l.get
+
+  def mapSt[A,B,C] (in: VStSF[A,B])(l: C @> B): VStSF[A,C] = {
+    def nextSt (s: State[B,Unit]): State[C,Unit] =
+      init[C] >>= (c ⇒ l := s.exec(l get c)) void
+
+    in map (_ map nextSt)
+  }
+
 //  def longIn[A](
 //    t: TextField,
 //    v: EndoVal[Long] = Validators.dummy[Long]
-//  )(l: A @> Long): VSET[A,A] = textIn[A,Long](t, v = v)(l)
+//  )(l: A @> Long): VStSF[A,A] = textIn[A,Long](t, v = v)(l)
 //
 //  def readVals[A:Read](
 //    t: TextField,
@@ -52,13 +49,13 @@ trait WidgetsFunctions extends ValidationFunctions {
 //  def stringIn[A](
 //    t: TextField,
 //    v: EndoVal[String] = Validators.dummy[String]
-//  )(l: A @> String): VSET[A,A] = textIn[A,String](t, v = v)(l)
+//  )(l: A @> String): VStSF[A,A] = textIn[A,String](t, v = v)(l)
 //
 //  def textIn[A,B:Read](
 //    t: TextField,
 //    v: EndoVal[B] = Validators.dummy[B],
 //    str: B ⇒ String = (b: B) ⇒ b.toString
-//  )(l: A @> B): VSET[A,A] =
+//  )(l: A @> B): VStSF[A,A] =
 //    lensed(readVals(t, str) andThen revalidate(v))(l)
 }
 
