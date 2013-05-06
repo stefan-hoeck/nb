efc432ffff97aafbed6550b851f444258e8644df
stefan-hoeck <efasckenoth@gmail.com>
2013-05-06 07:01:35 +0200

    cleaned up PureLookupTest

--- nb/src/test/scala/efa/nb/PureLookupTest.scala
+++ nb/src/test/scala/efa/nb/PureLookupTest.scala
@@ -1,21 +1,20 @@
 package efa.nb
 
-import dire._
+import dire._, DataSink.buffer
 import org.openide.util.lookup.{AbstractLookup, InstanceContent}
 import org.scalacheck._, Prop._
 import scalaz._, Scalaz._, scalacheck.ScalaCheckBinding._, effect.IO
 
 object PureLookupTest extends Properties("PureLookup") {
-  class Cc(override val toString: String)
+  case class Cc(t: Time)
 
-  implicit val CcEqual: Equal[Cc] = Equal.equalBy(_.toString)
-  implicit val CcArbitrary = Arbitrary(Gen.identifier ∘ (new Cc(_)))
+  implicit val CcEqual: Equal[Cc] = Equal.equalBy(_.t)
 
   property("LookupResults") =  {
     val ccs = new collection.mutable.ListBuffer[List[Cc]]
-    def sf(pl: PureLookup) =
-      (SF.time map { t ⇒ new Cc(t.toString) } syncTo pl.add[Cc]) >>
-      (pl.results[Cc] syncTo { xs ⇒ IO(ccs += xs) } count)
+
+    def sf(pl: PureLookup) = (SF.time map Cc syncTo pl.add[Cc]) >>
+                             (pl.results[Cc] to buffer(ccs) count)
 
     PureLookup() >>= { pl ⇒ SF.run(sf(pl), 2, 1L)(_ >= 10) } unsafePerformIO
 
